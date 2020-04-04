2ff5b0e454a3c6857442ed934af94fda66f2e438
Juveria Manzar <juveriadot@gmail.com>
2020-04-04 09:01:58 +0530

    updating theme impl

--- js/main.js
+++ js/main.js
@@ -56,8 +56,8 @@ function updateTasks() {
 
 function onLoad() {
     updateTasks();
-    // updateTheme(loadData("ToDoTheme"));
-    // document.body.style.display = "flex";
+    updateTheme(loadData("ToDoTheme"));
+    document.body.style.display = "flex";
 }
 
 
@@ -85,8 +85,39 @@ function deleteTaskOnClick(elem) {
             });
         });
     });
+}
+
+function updateTheme(theme) {
+    let bgColor = theme == 'light' ? "255, 255, 255" : "19, 19, 19";
+    let textColor = theme == 'light' ? "12, 12, 12" : "255, 255, 255";
+    let shadowColor = theme == 'light' ? "0, 0, 0" : "255, 255, 255";
+    let grad1 = theme == 'light' ? "108, 29, 103" : "34, 208, 163";
+    let grad2 = theme == 'light' ? "100, 25, 148" : "32, 173, 211";
+    let sideGrad1 = theme == 'light' ? "255, 255, 255" : "35, 35, 35";
+    let sideGrad2 = theme == 'light' ? "251, 247, 247" : "46, 46, 46";
+
+    let root = document.documentElement;
+
+    root.style.setProperty("--bg-color", bgColor);
+    root.style.setProperty("--text-color", textColor);
+    root.style.setProperty("--shadow-color", shadowColor);
+    root.style.setProperty("--gradient-1", grad1);
+    root.style.setProperty("--gradient-2", grad2);
+    root.style.setProperty("--sidebar-gradient-1", sideGrad1);
+    root.style.setProperty("--sidebar-gradient-2", sideGrad2);
+
+    document.getElementsByClassName('current-theme')[0].classList.remove('current-theme')
+
+    let activateClass = theme == 'light' ? 'light' : 'dark'
+    document.getElementById(activateClass).classList.add('current-theme')
+
+    saveData('ToDoTheme', theme)
 
 
+    let invertStrength = theme == 'light' ? "0%" : "100%"
+    let icons = document.getElementsByClassName('icon')
 
-
+    for (let i = 0; i < icons.length; i++) {
+        icons[i].style.filter = `brightness(100%) invert(${invertStrength})`
+    }
 }
